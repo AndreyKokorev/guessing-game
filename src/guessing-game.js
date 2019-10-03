1cccd1d23db472255e4e949a5e3640c5e6e68b91
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2019-10-03 08:47:34 +0700

    init: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+        this.minIndex;
+        this.maxIndex;
+    }
 
     setRange(min, max) {
-
+        this.minIndex = min;
+        this.maxIndex = max;
     }
 
     guess() {
-
+        
+        return Math.ceil((this.maxIndex + this.minIndex) / 2);
     }
 
     lower() {
-
+        this.maxIndex = Math.ceil((this.maxIndex + this.minIndex) / 2);
     }
 
     greater() {
-
+        this.minIndex = Math.ceil((this.maxIndex + this.minIndex) / 2);
     }
 }
 
